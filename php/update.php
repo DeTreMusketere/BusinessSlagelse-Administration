4f13d40e0d8c655f54b1c582c315d9265eaab35a
Patrick Kann Laptop <pkkann@gmail.com>
2014-11-26 18:33:24 +0100

    Ny og forbedre Update script ^
    Nu er update scripted dynamisk. Husk at skrive semikolon rundt om
    strings, da den ikke selv tjekker datatypen

--- php/update.php
+++ php/update.php
@@ -11,13 +11,27 @@
 	$whereColumn = $request->whereColumn;
 	$whereData = $request->whereData;
 
-	$name = $values[0];
-	$username = $values[1];
-	$email = $values[2];
-	$phone = $values[3];
-	$administrator = $values[4];
+	$sql = "UPDATE " . $table . " SET ";
 
-	$sql = "UPDATE " . $table . " SET name=" . $name . ", username=" . $username . ", email=" . $email . ", phone=" . $phone . ", administrator=" . $administrator . " WHERE " . $whereColumn[0] . "=" . $whereData[0];
+	$c = count($columns);
+
+	for($i = 0; $i < $c; $i++) {
+		$sql .= $columns[$i] . "=" . $values[$i];
+
+		if($i != ($c - 1)) {
+			$sql .= ", ";
+		}
+	}
+
+	$sql .=" WHERE " . $whereColumns[0] . "=" . $whereData[0];
+
+	$c1 = count($whereColumns);
+
+	if($c1 > 1) {
+		for($i = 1; $i < $c1; $i++) {
+			$sql .= " AND " . $whereColumns[$i] . "=" . $whereData[$i];
+		}
+	}
 	
 	if($conn->query($sql) === true) {
 		echo true;
