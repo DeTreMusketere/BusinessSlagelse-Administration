3e0467f5a38418344136c94aca82bc44d871a171
Astrinaar <7h380x@gmail.com>
2014-11-27 15:28:16 +0100

    tilbudsservice

---
+++ application/services/saleService.js
@@ -0,0 +1,23 @@
+angular.module('app').service("SaleService", function(SQLService) {
+	var sales;
+
+	this.create = function(){
+
+	};
+
+	this.save = function() {
+
+	};
+
+	this.delete = function() {
+
+	};
+
+	this.get = function() {
+
+	};
+
+	this.getAll = function() {
+
+	};
+});
