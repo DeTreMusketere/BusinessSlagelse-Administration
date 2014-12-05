b1483c419f2edab4d39bbba90864c784a2376ce0
Patrick Kann Laptop <pkkann@gmail.com>
2014-12-05 03:09:55 +0100

    små rettelser

--- application/controllers/tilbudOversigtController.js
+++ application/controllers/tilbudOversigtController.js
@@ -1,4 +1,6 @@
 angular.module('app').controller('TilbudOversigtController', function($scope, SaleService) {
+	var l = Ladda.create( document.getElementById('buttonDelete') );
+
 	$scope.sales = [];
 
 	$scope.load = function() {
@@ -11,20 +13,27 @@ angular.module('app').controller('TilbudOversigtController', function($scope, Sa
 
 	// DeleteSale
 	$scope.saleToDelete;
+	$scope.deleting = false;
 
 	$scope.setSaleToDelete = function(sale) {
 		$scope.saleToDelete = sale;
 	};
 
 	$scope.delete = function() {
+		l.start();
+		$scope.deleting = true;
 		SaleService.delete($scope.saleToDelete,
 			function() {
 				$('#deleteModal').modal('hide');
 				$.simplyToast('Tilbudet blev slettet', 'success');
 				$scope.load();
+				l.stop();
+				$scope.deleting = false;
 			},
 			function() {
 				$.simplyToast('Tilbudet kunne ikke slettes', 'danger');
+				l.stop();
+				$scope.deleting = false;
 			});
 	};
 });
